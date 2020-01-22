ea381516a74bd5bb05be9eb1d832f88cef95d9d9
AninditaDe <aninditade2@gmail.com>
2020-01-22 17:19:56 +0000

    addingtestpackage

---
+++ test-doc/test.js
@@ -0,0 +1,5 @@
+var demo = require('npm-multipleurl-package');
+const urls = [  'https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/ftse-fsi.json',
+  'https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/gbp-hkd.json',
+  'https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/gbp-usd.json'  ];  
+demo(urls).then(data => console.log(data));
